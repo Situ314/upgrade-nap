bb517382d122ec5342e523f027804c5f1fb3fdde
Jesu Sanchez <jd_sanchez1993@hotmail.es>
2021-10-08 00:32:39 -0500

    Update ReservationController.php

--- app/Http/Controllers/v3/ReservationController.php
+++ app/Http/Controllers/v3/ReservationController.php
@@ -173,7 +173,6 @@ class ReservationController extends Controller
             ];
 
             $resrvationCreated = \App\Models\GuestCheckinDetails::create($reservationData);
-            $data[] = [$reservation['reservation_number'] => $resrvationCreated->sno];
             $this->saveLogTracker([
                 'module_id' => 8,
                 'action'    => 'add',
@@ -184,6 +183,7 @@ class ReservationController extends Controller
                 'hotel_id'  => $hotel_id,
                 'type'      => 'API-V3'
             ]);
+            $data[] = [$reservation['reservation_number'] => $resrvationCreated->sno];
         }
 
         try {
@@ -221,16 +221,25 @@ class ReservationController extends Controller
         // Capturar hotel id, por default el valor es null, en caso de no enviarlo
         $hotel_id = isset($request->hotel_id) ? $request->hotel_id : null;
 
+        // Se agrega guest_number, para poder crear reservas en caso de no ser encontrada, el campo no es requerido
+        // De no enviarse el campo, no se creara ningun registro nuevo
+        // -----
+        // Se quito la validacion de verificar que reservations.*.reservation_number ya se encuentre registrado, 
+        // para cuando no exista, crearlo
         $validator = Validator::make($request->all(), [
             'hotel_id' => 'required|numeric|exists:hotels',
+            'guest_number' => [
+                'string',
+                Rule::exists("integrations_guest_information")->where('hotel_id', $hotel_id)
+            ],
             "reservations" => "required|array",
             'reservations.*.reservation_status' => 'required|numeric|in:0,1,2,3', //0: reserved, 1:checked in, 2: cancelled, 3: checked out
-            'reservations.*.reservation_number' => [
-                'string',
-                'required',
-                'distinct',
-                Rule::exists('guest_checkin_details')->where('hotel_id', $hotel_id)
-            ],
+            // 'reservations.*.reservation_number' => [
+            //     'string',
+            //     'required',
+            //     'distinct',
+            //     Rule::exists('guest_checkin_details')->where('hotel_id', $hotel_id)
+            // ],
             "reservations.*.room" => [
                 'string',
                 'nullable',
@@ -245,9 +254,9 @@ class ReservationController extends Controller
 
         if ($validator->fails()) {
             return response()->json([
-                'status'        => "error",
-                'message'       => "Error during the validation of the information",
-                'errors'   => $validator->errors()
+                'status'    => "error",
+                'message'   => "Error during the validation of the information",
+                'errors'    => $validator->errors()
             ], 400);
         }
 
@@ -255,8 +264,8 @@ class ReservationController extends Controller
 
         if (!$this->validateHotelId($hotel_id, $staff_id)) {
             return response()->json([
-                "status"        => "error",
-                "message"       => "User does not have access to the hotel",
+                "status"    => "error",
+                "message"   => "User does not have access to the hotel",
                 'errors'    => null
             ], 400);
         }
@@ -264,8 +273,16 @@ class ReservationController extends Controller
         $this->configTimeZone($hotel_id);
         $reservations = $request->reservations;
 
+        $guest = null;
+        // Validar que exista guest_number, para poder crear las reserva nuevas
+        if (isset($request->guest_number)) {
+            $guest_number = $request->guest_number;
+            $guest = \App\Models\IntegrationsGuestInformation::where('hotel_id', $hotel_id)->where('guest_number', $guest_number)->first();
+        }
+
         foreach ($reservations as $key => $reservation) {
             $reservation_number = $reservation['reservation_number'];
+
             $findReservation = \App\Models\GuestCheckinDetails::where("hotel_id", $hotel_id)
                 ->where("reservation_number", $reservation_number)
                 ->first();
@@ -277,44 +294,72 @@ class ReservationController extends Controller
                 $room_id = $hotel_room["room_id"];
             }
 
-            if ($findReservation->room_id > 0 && $findReservation->room_id != $room_id && intval($findReservation->reservation_status) == 1) {
-                \App\Models\RoomMove::create([
-                    'guest_id'          => $findReservation->guest_id,
-                    'current_room_no'   => $findReservation->room_no,
-                    'new_room_no'       => $room_id,
-                    'hotel_id'          => $hotel_id,
-                    'created_by'        => $staff_id,
-                    'created_on'        => date("Y-m-d H:i:s"),
-                    'updated_on'        => null,
-                    'comment'           => '',
-                    'phone'             => '',
-                    'updated_by'        => 0,
+            // Validar si existe el registro, de no encontrarlo, crear el registro
+            if ($findReservation) {
+                if (($findReservation->room_no > 0 && ($findReservation->room_no != $room_id)) && $findReservation->reservation_status == 1) {
+                    \App\Models\RoomMove::create([
+                        'guest_id'          => $findReservation->guest_id,
+                        'current_room_no'   => $findReservation->room_no,
+                        'new_room_no'       => $room_id,
+                        'hotel_id'          => $hotel_id,
+                        'created_by'        => $staff_id,
+                        'created_on'        => date("Y-m-d H:i:s"),
+                        'updated_on'        => null,
+                        'comment'           => '',
+                        'phone'             => '',
+                        'updated_by'        => 0,
+                    ]);
+                }
+
+                $reservationData = [
+                    'room_no'               => $room_id,
+                    'check_in'              => $reservation["check_in"],
+                    'check_out'             => $reservation["check_out"],
+                    'reservation_number'    => $reservation['reservation_number'],
+                    'reservation_status'    => $reservation['reservation_status'],
+                    'comment'               => array_key_exists('comment', $reservation) ? $reservation['comment'] : "",
+                    'status'                => (intval($reservation["reservation_status"]) == 0 || intval($reservation["reservation_status"]) == 1) ? 1 : 0
+                ];
+
+                $findReservation->fill($reservationData);
+                $findReservation->save();
+
+                $this->saveLogTracker([
+                    'module_id' => 8,
+                    'action'    => 'add',
+                    'prim_id'   => $findReservation->sno,
+                    'staff_id'  => $staff_id,
+                    'date_time' => date("Y-m-d H:i:s"),
+                    'comments'  => 'RESERVATION UPDATE | ' . json_encode($findReservation),
+                    'hotel_id'  => $hotel_id,
+                    'type'      => 'API-V3'
                 ]);
+            } else {
+                if ($guest) {
+                    $reservationData = [
+                        'guest_id'              => $guest->guest_id,
+                        'hotel_id'              => $hotel_id,
+                        'room_no'               => $room_id,
+                        'check_in'              => $reservation["check_in"],
+                        'check_out'             => $reservation["check_out"],
+                        'reservation_number'    => $reservation['reservation_number'],
+                        'reservation_status'    => $reservation['reservation_status'],
+                        'comment'               => array_key_exists('comment', $reservation) ? $reservation['comment'] : "",
+                        'status'                => (intval($reservation["reservation_status"]) == 0 || intval($reservation["reservation_status"]) == 1) ? 1 : 0
+                    ];
+                    $resrvationCreated = \App\Models\GuestCheckinDetails::create($reservationData);
+                    $this->saveLogTracker([
+                        'module_id' => 8,
+                        'action'    => 'add',
+                        'prim_id'   => $resrvationCreated->sno,
+                        'staff_id'  => $staff_id,
+                        'date_time' => date("Y-m-d H:i:s"),
+                        'comments'  => 'RESERVATION CREATION | ' . json_encode($resrvationCreated),
+                        'hotel_id'  => $hotel_id,
+                        'type'      => 'API-V3'
+                    ]);
+                }
             }
-
-            $reservationData = [
-                'room_no'               => $room_id,
-                'check_in'              => $reservation["check_in"],
-                'check_out'             => $reservation["check_out"],
-                'reservation_number'    => $reservation['reservation_number'],
-                'reservation_status'    => $reservation['reservation_status'],
-                'comment'               => array_key_exists('comment', $reservation) ? $reservation['comment'] : "",
-                'status'                => (intval($reservation["reservation_status"]) == 0 || intval($reservation["reservation_status"]) == 1) ? 1 : 0
-            ];
-
-            $findReservation->fill($reservationData);
-            $findReservation->save();
-
-            $this->saveLogTracker([
-                'module_id' => 8,
-                'action'    => 'add',
-                'prim_id'   => $findReservation->sno,
-                'staff_id'  => $staff_id,
-                'date_time' => date("Y-m-d H:i:s"),
-                'comments'  => 'RESERVATION UPDATE | ' . json_encode($findReservation),
-                'hotel_id'  => $hotel_id,
-                'type'      => 'API-V3'
-            ]);
         }
 
         return response()->json([
