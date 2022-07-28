2701b2bc260f088947b3e05b0df10ce6a4c8c3b9
Shift <shift@laravelshift.com>
2022-07-28 18:01:08 +0000

    Apply Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-12 coding style as a base with some minor additions.
    
    You may customize the code style applied by configuring [Pint](https://laravel.com/docs/pint), [PHP CS Fixer](https://github.com/FriendsOfPHP/PHP-CS-Fixer), or [PHP CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) for your project root.
    
    For more information on customizing the code style applied by Shift, [watch this short video](https://laravelshift.com/videos/shift-code-style).

--- config/app.php
+++ config/app.php
@@ -189,7 +189,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         //Sentry\Laravel\ServiceProvider::class,
         Sentry\Laravel\ServiceProvider::class,
-       // Sentry\SentryLaravel\SentryLaravelServiceProvider::class,
+        // Sentry\SentryLaravel\SentryLaravelServiceProvider::class,
         // Amazon SQS
         Aws\Laravel\AwsServiceProvider::class,
         // Amazon SQS FIFO
